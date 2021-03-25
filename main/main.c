373435b3a7d801ae895e6b33116cf5d0a02bb622
633706 <633706@unziar.es>
2021-03-25 21:06:07 +0100

    Commit entrega primer punto, con FreeRTOS se crean dos tareas. Una1parpadea un LED con 200ms de período, y otra envía cada segundo un mensaje por puerto serie.

--- main/main.c
+++ main/main.c
@@ -1,53 +1,27 @@
 #include "main.h"
 
-void app_main(){
-	UART_init();
-	LED_init ();
-	MPU9250_init();
-	MPU9250_WhoAmI(&WhoAmI_);
-	if (WhoAmI_ == 0x71){
-		TIM_init(100,200);
-		sprintf(msgTx_PC,"NO ERROR\r\n");		
-	}else{
-		sprintf(msgTx_PC,"ERROR, not running");		
-	}
-
-
-	
+static void LED_task (void *arg)
+{
+ 	LED_init();  
     while (1) {
-		MPU9250_get_acc(&accX,&accY,&accZ);
-	}
-}
-
-//Configured to 100ms, to sample
- void callback_ISR_G0T0 (void){
-	
-	accX_[samples] = accX;
-	accY_[samples] = accY;
-	accZ_[samples] = accZ;
-	samples ++;
-	
-	//after 10 samples send data
- 	if (samples == 10){
-		while (samples != 0){
-			accX_mean += accX_[samples];
-			accY_mean += accY_[samples];
-			accZ_mean += accZ_[samples];
-			samples --;
-		}
-		accX_mean /= 10;
-		accY_mean /= 10;
-		accZ_mean /= 10;
-		sprintf(msgTx_PC,"X: %d\r\nY: %d\r\nZ: %d\r\n\r\n",accX_mean,accY_mean,accZ_mean);
-		UART_send(msgTx_PC);
 		LED_ON();
-		TIM_resume();
-		
-	}
+        vTaskDelay(pdMS_TO_TICKS(100));
+		LED_OFF();
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
 }
 
-//configured to turn off LED after 200 ms.
-void callback_ISR_G0T1 (void){
-	LED_OFF();
-	TIM_stop();
+static void pcCOM_task(void *arg)
+{
+	UART_init();
+    while (1) {
+		UART_send(msgTx_PC);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+void app_main(void)
+{
+	xTaskCreatePinnedToCore(LED_task, "LED", 1024, NULL, 2, NULL, tskNO_AFFINITY);
+    xTaskCreatePinnedToCore(pcCOM_task, "pcCOM", 1024, NULL, 3, NULL, tskNO_AFFINITY);
 }
